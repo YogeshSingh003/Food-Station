d5435a5bdecc981853527fffb38e6c541e953d69
Yogesh Singh <yogeshsinghbhati03@gmail.com>
2024-05-05 23:52:02 +0530

    Learning

--- App.js
+++ App.js
@@ -2,8 +2,24 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 const HeadingComponent = () => {
-  return <h1>Namaste React 🚀</h1>;
+  return (
+    <h1>
+      Namaste React 🚀
+      <FirstComponent> </FirstComponent>
+    </h1>
+  );
 };
+
+function FirstComponent() {
+  return (
+    <h1>
+      First Component
+      <SecondComponent></SecondComponent>
+    </h1>
+  );
+}
+
+const SecondComponent = () => <h1> SecondComponent</h1>;
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<HeadingComponent></HeadingComponent>);
