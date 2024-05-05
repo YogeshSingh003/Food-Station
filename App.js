d4b7e814844f8fbf8f08e6b539f6b7c956a1c48d
Yogesh Singh <yogeshsinghbhati03@gmail.com>
2024-05-05 23:01:41 +0530

    Learning

--- App.js
+++ App.js
@@ -1,2 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+
+const HeadingComponent = () => {
+  return <h1>Namaste React 🚀</h1>;
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<HeadingComponent></HeadingComponent>);
