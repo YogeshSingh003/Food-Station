9d0cfec976796bdfeb3dd2a4a1fb2c3d2fbdefc8
Yogesh Singh <yogeshsinghbhati03@gmail.com>
2024-05-07 22:26:18 +0530

    TopRatedFeature

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,16 +1,33 @@
 import RestaurantCard from "./RestaurantCard";
 import resList from "../../utils/mockData";
+import { useState } from "react";
 
 const Body = () => {
+  const [restaurantList, setRestaurantList] = useState(resList);
   return (
     <div className="body">
-      <div className="search"> Search</div>
+      {/* <div className="search"> Search</div> */}
+      <div>
+        <button
+          className="res-btn"
+          onClick={() => {
+            const filterList = restaurantList.filter(
+              (res) => res.info.avgRating > 4
+            );
+
+            setRestaurantList(filterList);
+          }}
+        >
+          Top rated restaurant
+        </button>
+        <button onClick={() => setRestaurantList(resList)}>Reset</button>
+      </div>
       <div className="res-container">
         {/** Different styles of mapping */}
         {/* {resList.map((item, index) => (
             <RestaurantCard resData={resList[index]} />
           ))} */}
-        {resList.map((restaurant, index) => (
+        {restaurantList.map((restaurant) => (
           <RestaurantCard key={restaurant.info.id} resData={restaurant} />
         ))}
       </div>
