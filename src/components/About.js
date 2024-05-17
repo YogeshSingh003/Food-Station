8ca65b9ce805c0e1176a79c635782640bfdabfe6
Yogesh Singh <yogeshsinghbhati03@gmail.com>
2024-05-17 18:24:31 +0530

    About update

--- src/components/About.js
+++ src/components/About.js
@@ -2,12 +2,15 @@ import { useState, useEffect } from "react";
 import { useFetch } from "../../utils/useFetch";
 
 const About = () => {
-  const { data } = useFetch("https://api.github.com/users/YogeshSingh003");
-  console.log(data);
+  const data = useFetch("https://api.github.com/users/YogeshSingh003");
+
+  useEffect(() => {
+    console.log(data.response);
+  }, [data]);
   return (
     <>
       <h2>About</h2>
-      <div>{data && data.map((name) => <p key={name.id}>{name.login}</p>)}</div>
+      {/* <div>{data.response.name}</div> */}
     </>
   );
 };
