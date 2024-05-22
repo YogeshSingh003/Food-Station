b163964c32098f20bddc482c5ef976cfc7271a8e
Yogesh Singh <yogeshsinghbhati03@gmail.com>
2024-05-22 14:29:37 +0530

    Accordian

---
+++ src/components/RestaurantCategory.js
@@ -0,0 +1,28 @@
+import { useState } from "react";
+import ItemList from "./ItemList";
+const RestaurantCategory = ({ cat }) => {
+  //   const { title } = cat?.card?.card;
+
+  const [showItems, setShowItems] = useState(true);
+  handleClick = () => {
+    setShowItems(!showItems);
+  };
+  return (
+    <>
+      <div className="w-1/2 mx-auto my-4 p-4 bg-gray-50 shadow-lg  ">
+        <div
+          className="flex justify-between cursor-pointer"
+          onClick={handleClick}
+        >
+          <span className="font-semibold">
+            {cat.title}({cat.itemCards.length})
+          </span>
+          <span className="text-2xl">⬇️</span>
+        </div>
+        <div>{showItems || <ItemList items={cat.itemCards} />}</div>
+      </div>
+    </>
+  );
+};
+
+export default RestaurantCategory;
