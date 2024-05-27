b5ab446eaa15a89d03288af1af1742652ea47332
Yogesh Singh <84557278+YogeshSingh003@users.noreply.github.com>
2024-05-27 11:19:28 +0530

    reverse CORS update

--- src/components/Body.js
+++ src/components/Body.js
@@ -16,7 +16,7 @@ const Body = () => {
   }, []);
   const fetchData = async () => {
     const data = await fetch(
-      "https://corsproxy.io/?https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.4594965&lng=77.0266383&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.4594965&lng=77.0266383&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
     );
     const json = await data.json();
 
