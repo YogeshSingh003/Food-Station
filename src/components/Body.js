da0cb5547af12f35639fa5eb6565f9329569ae46
Yogesh Singh <yogeshsinghbhati03@gmail.com>
2024-05-27 11:26:03 +0530

    update API

--- src/components/Body.js
+++ src/components/Body.js
@@ -22,7 +22,7 @@ const Body = () => {
   //   const json = await data.json();
   const fetchData = async () => {
     const data = await fetch(
-      "https://corsproxy.io/?https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.4594965&lng=77.0266383&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+      "https://foodfire.onrender.com/api/restaurants?lat=28.4594965&lng=77.0266383&page_type=DESKTOP_WEB_LISTING"
     );
     const json = await data.json();
 
